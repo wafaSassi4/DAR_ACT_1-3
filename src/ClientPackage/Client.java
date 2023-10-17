0ccb76ec94cdc184394e5e196ecd3e82bcd698bf
wafas <wafas@LAPTOP-TC93U6IS>
2023-10-17 17:03:57 +0100

    act 1.3

--- src/ClientPackage/Client.java
+++ src/ClientPackage/Client.java
@@ -1,43 +1,93 @@
 package ClientPackage;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.net.InetAddress; 
+import java.net.InetSocketAddress; 
+import java.net.Socket; 
+import java.util.Scanner; 
+ 
+ class Client { 
+   public static void main(String[] args) { 
+       try { 
+           int rep, choice = 0; 
+           System.out.println("I am a client not yet connected ... "); 
+ 
+           InetAddress serverInetAddress = InetAddress.getByName("192.168.56.1"); 
+           InetSocketAddress serverSocketAddress = new 
+           InetSocketAddress(serverInetAddress, 1234); 
+ 
+           Socket socket = new Socket(); 
+           socket.connect(serverSocketAddress); 
+ 
+           System.out.println("Establish a connection to the server"); 
+ 
+           InputStream inputStream = socket.getInputStream(); 
+           OutputStream outputStream = socket.getOutputStream(); 
+ 
+           //Reading a number 
+           Scanner scanner = new Scanner(System.in); 
+           System.out.println("Give a number:"); 
+           int nb = scanner.nextInt(); 
 
-public class Client {
-
-	public static void main(String[] args) throws IOException{
-		
-		try {
-			int serverPort=1234;
-			Socket socket= new Socket("localhost",serverPort);
-			
-			System.out.println("je suis un client connecté");
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Donner un nombre : ");
-            int i =sc.nextInt();
-			OutputStream os=socket.getOutputStream(); 
-			os.write(i);
-			InputStream is=socket.getInputStream();
-			int nb=is.read();
-			
-			System.out.println("le Resultat : "+nb);
-			socket.close();
-		} catch (IOException e) {
-		
-			e.printStackTrace();
-		}
-				
-	}
-	
-	
-		  
-	
-	
-
-	}
-
-	
-
-
+           outputStream.write(nb); 
+           System.out.println("I send the number " +nb+" to the server"); 
+ 
+           //Operation menu 
+           do{ 
+               System.out.println("Menu:"); 
+               System.out.println("1. Addition"); 
+               System.out.println("2. Substraction"); 
+               System.out.println("3. Multiplication"); 
+               System.out.println("4. Division"); 
+ 
+               System.out.print("Enter your choice: "); 
+ 
+               if (scanner.hasNextInt()) { 
+                   choice = scanner.nextInt(); 
+                   scanner.nextLine(); 
+                   if(choice<1 || choice>4) 
+                       System.out.println("Invalid input. Please enter a number corresponding to your choice."); 
+ 
+               } 
+ 
+           }while(choice<1 || choice>4); 
+ 
+           switch (choice) { 
+               case 1: 
+                   System.out.println("You selected Addition operation"); 
+                   outputStream.write(1); 
+                   System.out.println("I'm waiting for the response from the server"); 
+                   rep = inputStream.read(); 
+                   System.out.println("Response = "+nb+ " + 5 = "+rep); 
+                   break; 
+               case 2: 
+                   System.out.println("You selected Substraction operation"); 
+                   outputStream.write(2); 
+                   System.out.println("I'm waiting for the response from the server"); 
+                   rep = inputStream.read(); 
+                   System.out.println("Response = "+nb+ " - 5 = "+rep); 
+                   break; 
+               case 3: 
+                   System.out.println("You selected Multiplication operation"); 
+                   outputStream.write(3); 
+                   System.out.println("I'm waiting for the response from the server"); 
+                   rep = inputStream.read(); 
+                   System.out.println("Response = "+nb+ " * 5 = "+rep); 
+                   break; 
+               case 4: 
+            	   System.out.println("You selected Division operation"); 
+            	   outputStream.write(4); 
+            	   System.out.println("I'm waiting for the response from the server"); 
+            	   rep = inputStream.read(); 
+            	   System.out.println("Response = "+nb+ " / 5 = "+rep); 
+            	   break; 
+               default: 
+            	   System.out.println("Invalid choice. Please select a valid option."); 
+           } 
+           System.out.println("Closing the client socket..."); 
+           socket.close(); 
+       } catch (Exception e) { 
+    	   e.printStackTrace(); 
+       } 
+   } 
+ }
--- src/ServerPackage/Server.java
+++ src/ServerPackage/Server.java
@@ -1,39 +1,58 @@
 package ServerPackage;
-
-import java.io.IOException;
+ 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
-
-public class Server {
-
-	public static void main(String[] args) throws IOException {
-		try {
-			int serverPort=1234;
-			ServerSocket serverSocket = new ServerSocket(serverPort);
-			System.out.println("je suis un serveur en attente la conexion d'un client");
-			Socket s =serverSocket.accept();
-			
-			InputStream is=s.getInputStream();
-			int nb=is.read()*5;
-			System.out.println("la valeur a envoye a client : "+nb);
-			OutputStream os=s.getOutputStream(); 
-			os.write(nb);
-			
-			s.close();
-		} catch (IOException e) {
-		
-			e.printStackTrace();
-		}
-				
-	}
-	
-	
-		 
-	
-
-	}
-	
-
-
+import java.net.Socket; 
+ class Server { 
+public static void main (String[] args) { 
+try { 
+         ServerSocket serverSocket = new ServerSocket(1234); 
+         System.out.println("I am a server waiting for a client connexion..."); 
+         Socket socket = serverSocket.accept(); 
+         
+         InputStream inputStream = socket.getInputStream(); 
+         OutputStream outputStream = socket.getOutputStream();
+         
+         System.out.println("Waiting for a number ... "); 
+         int nb = inputStream.read(); 
+         System.out.println("Data received : " + nb); 
+         int choice = inputStream.read(); 
+         int rep; 
+   switch (choice) { 
+               case 1: 
+                   System.out.println("The client selected Addition operation"); 
+                   rep = nb + 5; 
+                   outputStream.write(rep); 
+                   break; 
+               case 2: 
+                   System.out.println("The client selected Soustraction operation"); 
+                   rep = nb - 5; 
+                   outputStream.write(rep); 
+                   break; 
+               case 3: 
+                   System.out.println("The client selected Multiplication operation"); 
+                   rep = nb * 5; 
+                   outputStream.write(rep); 
+                   break; 
+               case 4: 
+                   System.out.println("The client selected Division operation"); 
+                   rep = nb / 5; 
+                   outputStream.write(rep); 
+                   break; 
+               default: 
+                   System.out.println("Return to Client."); 
+           } 
+ 
+ 
+           System.out.println("Closing the socket..."); 
+ 
+           socket.close(); 
+           serverSocket.close(); 
+ 
+       } catch (Exception e) { 
+           e.printStackTrace(); 
+       } 
+   } 
+} 
+ 
